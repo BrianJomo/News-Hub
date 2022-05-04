31abbbf30196bff986ebbd6dbd5b1d8b9fca5afe
Brian Jomo <98019316+BrianJomo@users.noreply.github.com>
2022-05-04 08:46:04 +0300

    It will update the entire file.

---
+++ app/models.py
@@ -0,0 +1,27 @@
+class NewsArticle:
+    def __init__(self, source_name, author, title, url, url_to_image, published_at, description, content):
+        '''
+        A function that initializes the news article and its parameters
+
+        Args:
+            source_name, author, title, url, url_to_image, published_at, description, content
+        '''
+        self.source_name=source_name
+        self.author=author
+        self.title=title
+        self.url=url
+        self.url_to_image=url_to_image
+        self.published_at=published_at
+        self.description=description
+        self.content=content
+
+class Sources:
+    def __init__(self, source_id, source_name):
+        '''
+        Function that initializes the article source
+
+        Args:
+            source_id, source_name
+        '''
+        self.source_id=source_id
+        self.source_name=source_name
